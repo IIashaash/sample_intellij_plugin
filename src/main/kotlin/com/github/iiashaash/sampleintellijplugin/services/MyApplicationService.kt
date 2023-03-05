c6ea4cde40b953395e4f7ee65ddd8ce5682a83e1
GitHub Action <action@github.com>
2023-03-05 17:21:34 +0000

    Template cleanup

--- src/main/kotlin/com/github/iiashaash/sampleintellijplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/iiashaash/sampleintellijplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.iiashaash.sampleintellijplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.iiashaash.sampleintellijplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/iiashaash/sampleintellijplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/iiashaash/sampleintellijplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.iiashaash.sampleintellijplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.iiashaash.sampleintellijplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
